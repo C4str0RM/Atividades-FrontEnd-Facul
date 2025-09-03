3b634394bded86a84e43a5f1c503ce8a94f34db7
Raycka Castro <messacastrottoo@gmail.com>
2025-09-03 19:58:25 -0400

    segunda atividade

---
+++ src/atividade.js
@@ -0,0 +1,140 @@
+// 10 Strings
+let fruta = "Banana"
+let cor = "Preto"
+let cidade = "Rolim de Moura"
+let linguagem = "JavaScript"
+let animal = "Cachorro"
+let planeta = "Terra"
+let nome = "Raycka"
+let musica = "Internacional"
+let esporte = "Vôlei"
+let carro = "Fiat FastBack"
+
+// Validações com switch
+switch(fruta) {
+  case "Banana":
+    console.log("Fruta é Banana!")
+    break
+  default:
+    console.log("Fruta desconhecida")
+}
+
+switch(cor) {
+  case "Azul":
+    console.log("Cor favorita é Preto!")
+    break
+  default:
+    console.log("Outra cor")
+}
+
+switch(cidade) {
+  case "Rolim de Moura":
+    console.log("Você mora em Rondônia!")
+    break
+  default:
+    console.log("Cidade não reconhecida")
+}
+
+switch(linguagem) {
+  case "JavaScript":
+    console.log("Você ama JS!")
+    break
+  default:
+    console.log("Outra linguagem")
+}
+
+switch(animal) {
+  case "Gato":
+    console.log("Animal favorito é Cachorro!")
+    break
+  default:
+    console.log("Outro animal")
+}
+
+switch(planeta) {
+  case "Terra":
+    console.log("Você vive na Terra!")
+    break
+  default:
+    console.log("Planeta desconhecido")
+}
+
+switch(nome) {
+  case "Raycka":
+    console.log("Olá, Raycka!")
+    break
+  default:
+    console.log("Nome não reconhecido")
+}
+
+switch(musica) {
+  case "Pop":
+    console.log("Gosta de música Internacional!")
+    break
+  default:
+    console.log("Outro estilo musical")
+}
+
+switch(esporte) {
+  case "Futebol":
+    console.log("Esporte favorito é Vôlei!")
+    break
+  default:
+    console.log("Outro esporte")
+}
+
+switch(carro) {
+  case "Civic":
+    console.log("Carro é um Fiat Fastback!")
+    break
+  default:
+    console.log("Outro carro")
+}
+
+// 10 Numbers
+let idade = 20
+let altura = 1.65
+let peso = 60
+let pontos = 100
+let nivel = 5
+let temperatura = 30
+let velocidade = 80
+let saldo = 250.75
+let ano = 2025
+let nota = 9.5
+
+// Validações com if
+if (idade >= 18) console.log("Maior de idade")
+if (altura < 1.70) console.log("Altura abaixo de 1.70m")
+if (peso > 50) console.log("Peso acima de 50kg")
+if (pontos >= 100) console.log("Pontuação máxima atingida!")
+if (nivel === 5) console.log("Você está no nível 5")
+if (temperatura > 25) console.log("Está calor!!")
+if (velocidade > 60) console.log("Velocidade acima do permitido!!ATENÇÃO!!")
+if (saldo > 0) console.log("Saldo positivo!")
+if (ano === 2025) console.log("Estamos em 2025 ")
+if (nota >= 7) console.log("Nota suficiente para passar ")
+
+// 10 Booleanos
+let estaLogado = true
+let temCarteira = false
+let gostaDeChocolate = true
+let estaChovendo = false
+let terminouTarefa = true
+let estaOnline = true
+let temAnimal = false
+let gostaDeMusica = true
+let estaComFome = false
+let estaEstudando = true
+
+// Validações com if
+if (estaLogado) console.log("Usuário está logado!!")
+if (!temCarteira) console.log("Sem carteira de motorista!")
+if (gostaDeChocolate) console.log("Ama chocolate")
+if (!estaChovendo) console.log("Dia está seco")
+if (terminouTarefa) console.log("Tarefa concluída!")
+if (estaOnline) console.log("Usuário online")
+if (!temAnimal) console.log("Não tem pet")
+if (gostaDeMusica) console.log("Curte música")
+if (!estaComFome) console.log("Sem fome agora")
+if (estaEstudando) console.log("Está estudando")
